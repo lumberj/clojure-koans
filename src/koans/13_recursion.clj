1d5d56434aab36daa6377bea7fea8b478082dcd0
Alan Cohen <acohen@climate.com>
2013-05-05 20:49:00 -0700

    Recursion chapter complete

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,28 @@
 (defn is-even? [n]
   (if (= n 0)
     true
-    (___ (is-even? (dec n)))))
+    (= false (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      (= true acc)
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [n coll
+        acc '()]
+    (if (= 0 (count n))
+     acc
+     (recur (rest n) (conj acc (first n))))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+        acc 1]
+    (if (= 0 n)
+      acc
+      (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
